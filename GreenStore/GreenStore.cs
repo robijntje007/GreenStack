dca289dcfd4885f865853eda74d47b72a6e5c597
Rob Desair <robdesair@gmail.com>
2023-03-28 21:08:44 +0200

    Initial commit

---
+++ GreenStore/GreenStore.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace GreenStoreKata
+{
+    public class GreenStore
+    {
+        IList<Item> Items;
+        public GreenStore(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Cheddar" && Items[i].Name != "VIP tickets to a Samurai concert")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Longclaw")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "VIP tickets to a Samurai concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Longclaw")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Cheddar")
+                    {
+                        if (Items[i].Name != "VIP tickets to a Samurai concert")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Longclaw")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GreenStoreTests/GreenStoreTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GreenStoreKata;
+
+namespace GreenStoreTests
+{
+    public class GreenStoreTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GreenStore app = new GreenStore(Items);
+            app.UpdateQuality();
+            Assert.Equal("fixme", Items[0].Name);
+        }
+    }
+}
